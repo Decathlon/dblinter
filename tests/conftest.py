4349c7002f1a5bbd899975783a74c15ffdeab121
Pierre-Marie Petit <pierremarie.petit.partner@decathlon.com>
2024-11-24 16:06:21 +0100

    add rul T012

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 from testcontainers.core.waiting_utils import wait_container_is_ready
 from testcontainers.postgres import PostgresContainer
 
-PG_IMAGE = "postgres:14"
+PG_IMAGE = "registry.gitlab.com/dalibo/postgresql_anonymizer:latest"
 PG_PORT = 5432
 PG_USER = "postgres"
 PG_PASSWORD = "postgres"
