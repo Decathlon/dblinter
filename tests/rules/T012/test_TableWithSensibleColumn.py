5c41c41acf77b9aacc3469cae5790f9a5335d47c
Pierre-Marie Petit <pierremarie.petit.partner@decathlon.com>
2024-11-25 10:48:53 +0100

    add rul T012

--- tests/rules/T012/test_TableWithSensibleColumn.py
+++ tests/rules/T012/test_TableWithSensibleColumn.py
@@ -3,7 +3,6 @@ from dblinter.database_connection import DatabaseConnection
 from dblinter.function_library import FunctionLibrary
 from dblinter.sarif_document import SarifDocument
 
-
 def test_table_with_sensitive_column(postgres_instance_args) -> None:
     args = postgres_instance_args
     context = Context(
@@ -11,7 +10,7 @@ def test_table_with_sensitive_column(postgres_instance_args) -> None:
         fixes=[
             "Install extension anon, and create some masking rules on.",
         ],
-        message="{0} have column {1} (category {2}) that can be consider has sensitive. They should be masked, in non-prod environment."
+        message="{0} have column {1} (category {2}) that can be consider has sensitive. It should be masked for non data-operator users."
     )
     function_library = FunctionLibrary()
     db = DatabaseConnection(args)
@@ -23,5 +22,28 @@ def test_table_with_sensitive_column(postgres_instance_args) -> None:
     )
     assert (
         sarif_document.sarif_doc.runs[0].results[0].message.text
-        == "postgres.public.test have column creditcard (category creditcard) that can be consider has sensitive. They should be masked, in non-prod environment."
+        == "postgres.public.test have column creditcard (category creditcard) that can be consider has sensitive. It should be masked for non data-operator users."
     )
+    assert (
+        sarif_document.sarif_doc.runs[0].results[1].message.text
+        == "postgres.public.test have column id (category account_id) that can be consider has sensitive. It should be masked for non data-operator users."
+    )
+
+def test_table_without_sensitive_column(postgres_instance_args) -> None:
+    args = postgres_instance_args
+    context = Context(
+        desc="Base on the extension anon (https://postgresql-anonymizer.readthedocs.io/en/stable/detection), show sensitive column.",
+        fixes=[
+            "Install extension anon, and create some masking rules on.",
+        ],
+        message="{0} have column {1} (category {2}) that can be consider has sensitive. It should be masked for non data-operator users."
+    )
+    function_library = FunctionLibrary()
+    db = DatabaseConnection(args)
+    db.query('select anon.init()')
+    db.query('CREATE TABLE test (test_id integer, description text)')
+    sarif_document = SarifDocument()
+    function_library.get_function_by_function_name("table_with_sensible_column")(
+        function_library, db, [], context, ("public", "test"), sarif_document
+    )
+    assert sarif_document.sarif_doc.runs[0].results == []
