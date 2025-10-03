1b51b5647bf87259e939795485d9a2560430dc28
Vincent Moreau <vincent.moreau@decathlon.com>
2025-10-03 15:45:57 +0200

    Fix text (thanks GW)

--- tests/rules/C003/test_PasswordEncryptionIsMd5.py
+++ tests/rules/C003/test_PasswordEncryptionIsMd5.py
@@ -4,16 +4,16 @@ from dblinter.function_library import FunctionLibrary
 from dblinter.sarif_document import SarifDocument
 
 
-def test_password_encryption_is_md5(
-    postgres_instance_args,
+def test_password_encryption_with_md5(
+    postgres12_instance_args,
 ) -> None:
-    args = postgres_instance_args
+    args = postgres12_instance_args
     function_library = FunctionLibrary()
     db = DatabaseConnection(args)
     param = [{"password_encryption": "md5"}]
     context = Context(
         desc="password_encryption is set to md5",
-        message="password_encryption is set to md5",
+        message="password_encryption is set to md5, this will prevent upgrade to Postgres 18",
         fixes=[
             "change password_encryption to scram-sha-256 and rotate users passwords",
         ],
@@ -23,6 +23,29 @@ def test_password_encryption_is_md5(
         "password_encryption_is_md5"
     )(function_library, db, param, context, sarif_document)
     assert (
+        len(sarif_document.sarif_doc.runs[0].results)>0 and
         sarif_document.sarif_doc.runs[0].results[0].message.text
         == "password_encryption is set to md5, this will prevent upgrade to Postgres 18"
+    )
+
+def test_password_encryption_without_md5(
+    postgres_instance_args,
+) -> None:
+    args = postgres_instance_args
+    function_library = FunctionLibrary()
+    db = DatabaseConnection(args)
+    param = [{"password_encryption": "md5"}]
+    context = Context(
+        desc="password_encryption is set to md5",
+        message="password_encryption is set to md5, this will prevent upgrade to Postgres 18",
+        fixes=[
+            "change password_encryption to scram-sha-256 and rotate users passwords",
+        ],
+    )
+    sarif_document = SarifDocument()
+    function_library.get_function_by_function_name(
+        "password_encryption_is_md5"
+    )(function_library, db, param, context, sarif_document)
+    assert (
+        len(sarif_document.sarif_doc.runs[0].results)==0
     )
